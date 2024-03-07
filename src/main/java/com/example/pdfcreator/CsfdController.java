18ac81fddc759345233682a734060aba71a77947
Matěj <mckillem@tuta.io>
2024-03-07 10:36:27 +0100

    snaha o zpracování pdf

--- src/main/java/com/example/pdfcreator/CsfdController.java
+++ src/main/java/com/example/pdfcreator/CsfdController.java
@@ -1,12 +1,22 @@
 package com.example.pdfcreator;
 
 import com.example.pdfcreator.DTO.Csfd;
+import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/csfd")
@@ -16,8 +26,70 @@ public class CsfdController {
 
 //	@PostMapping(path="/...", produces = {MediaType.APPLICATION_PDF_VALUE})
 	@PostMapping(produces = {MediaType.APPLICATION_PDF_VALUE})
-	public String dataFromApi(@RequestBody Csfd csfd) {
-//		return csfdService.createPdf(csfd);
-		return "anoddddddd";
+//	public ResponseEntity<byte[]> dataFromApi(@RequestBody Csfd csfd) {
+	public Document dataFromApi(@RequestBody Csfd csfd) {
+		return csfdService.createPdf(csfd);
+	}
+
+
+	@RequestMapping(value="/getpdf", method= RequestMethod.POST)
+	public ResponseEntity<byte[]> getPDF(@RequestBody Csfd json) {
+		// convert JSON to Employee
+		Employee emp = convertSomehow(json);
+
+		// generate the file
+		showHelp(emp);
+
+		// retrieve contents of "C:/tmp/report.pdf" that were written in showHelp
+		byte[] contents = (...);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_PDF);
+		// Here you have to set the actual filename of your pdf
+		String filename = "output.pdf";
+		headers.setContentDispositionFormData(filename, filename);
+		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
+		return response;
+	}
+
+	public static Document showHelp(Employee emp) throws Exception {
+		Document document = new Document();
+
+		PdfWriter.getInstance(document, new FileOutputStream("C:/tmp/report.pdf"));
+		document.open();
+		document.add(new Paragraph("table"));
+		document.add(new Paragraph(new Date().toString()));
+		PdfPTable table=new PdfPTable(2);
+
+		PdfPCell cell = new PdfPCell (new Paragraph ("table"));
+
+		cell.setColspan (2);
+		cell.setHorizontalAlignment (Element.ALIGN_CENTER);
+		cell.setPadding (10.0f);
+		cell.setBackgroundColor (new BaseColor (140, 221, 8));
+
+		table.addCell(cell);
+		ArrayList<String[]> row=new ArrayList<String[]>();
+		String[] data=new String[2];
+		data[0]="1";
+		data[1]="2";
+		String[] data1=new String[2];
+		data1[0]="3";
+		data1[1]="4";
+		row.add(data);
+		row.add(data1);
+
+		for(int i=0;i<row.size();i++) {
+			String[] cols=row.get(i);
+			for(int j=0;j<cols.length;j++){
+				table.addCell(cols[j]);
+			}
+		}
+
+		document.add(table);
+		document.close();
+
+		return document;
 	}
 }
--- src/main/java/com/example/pdfcreator/CsfdService.java
+++ src/main/java/com/example/pdfcreator/CsfdService.java
@@ -1,14 +1,69 @@
 package com.example.pdfcreator;
 
 import com.example.pdfcreator.DTO.Csfd;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Service
 @AllArgsConstructor
 public class CsfdService {
 
-	public Object createPdf(Csfd data) {
-		return data;
+//	public ResponseEntity<byte[]> createPdf(Csfd data) {
+//	public ResponseEntity<byte[]> createPdf(Csfd data) {
+	public Document createPdf(Csfd data) {
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		// step 1: creation of a document-object
+//		Document document = new Document();
+//		// step 2:
+//		// we create 3 different writers that listen to the document
+//		PdfWriter pdf = PdfWriter.getInstance(document, baos);
+//
+//		// step 3: we open the document
+//		document.open();
+//		// step 4: we add a paragraph to the document
+//		document.add(new Paragraph("Hello World"));
+//		// we make references
+//		Anchor pdfRef = new Anchor("see Hello World in PDF.");
+//		pdfRef.setReference("./HelloWorldPdf.pdf");
+//		Anchor rtfRef = new Anchor("see Hello World in RTF.");
+//		rtfRef.setReference("./HelloWorldRtf.rtf");
+//
+//		// we add the references, but only to the HTML page:
+//
+//		pdf.pause();
+//		document.add(pdfRef);
+//		document.add(Chunk.NEWLINE);
+//		document.add(rtfRef);
+//		pdf.resume();
+//
+//		// step 5: we close the document
+//		document.close();
+
+		// step 1: creation of a document-object
+		Document document = new Document();
+		try {
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a PDF-stream to a file
+			PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+
+			// step 3: we open the document
+			document.open();
+			// step 4: we add a paragraph to the document
+			document.add(new Paragraph("Hello World"));
+		} catch (DocumentException de) {
+			System.err.println(de.getMessage());
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+
+		// step 5: we close the document
+		document.close();
+		return document;
 	}
 }
